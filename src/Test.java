6568c8e7ea095241db91d7d570f713154a063250
manoj <manoj.sarwate@ssiedu.in>
2020-01-20 19:41:24 +0530

    20-01-2020

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public void show(){
+        System.out.println("hello from show");
+    }
+    //entry-point of program execution
+    public static void main(String args){
+        System.out.println("hello from main");
+        
+    }
+}
+// varargs
